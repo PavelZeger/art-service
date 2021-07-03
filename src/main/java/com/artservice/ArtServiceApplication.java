5ec61f30dc896e1e3e1e8915752a644c47d10989
PavelZeger <gabberoid@gmail.com>
2021-07-03 22:32:04 +0300

    Encrypting password in application.properties

--- src/main/java/com/artservice/ArtServiceApplication.java
+++ src/main/java/com/artservice/ArtServiceApplication.java
@@ -1,5 +1,6 @@
 package com.artservice;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 @EnableAsync
 @EnableAspectJAutoProxy
 @EnableScheduling
