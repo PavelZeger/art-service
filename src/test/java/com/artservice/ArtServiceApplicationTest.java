4d375efa843403cc67582b1a0468d70d6fd54063
PavelZeger <gabberoid@gmail.com>
2021-07-05 21:24:03 +0300

    Updated .gitignore

---
+++ src/test/java/com/artservice/ArtServiceApplicationTest.java
@@ -0,0 +1,19 @@
+package com.artservice;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Pavel Zeger
+ * @implNote art-service
+ * @since 03/07/2021
+ */
+class ArtServiceApplicationTest extends AbstractTest {
+
+    @Test
+    public void shouldReturnDecryptedPassword() {
+        assert true;
+    }
+
+}
